f94421ec0766cf007f5658f790bfa7ef52cb51e4
TobiasStadler97 <tobias.stadler@hotmail.de>
2021-10-21 15:58:02 +0200

    UB-22 added (mappedBy = "professor") in Professor

--- src/main/java/users/Professor.java
+++ src/main/java/users/Professor.java
@@ -1,7 +1,14 @@
 package users;
 
+import domaindata.Course;
+
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 public class Professor extends Person {
+
+    @OneToMany(mappedBy = "professor")
+    private List<Course> courses;
 }

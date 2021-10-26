93bc0a4e9dffffce8687c1cb09564f45e7cf774f
TobiasStadler97 <tobias.stadler@hotmail.de>
2021-10-26 13:05:40 +0200

    UB-28: added created attribute to Comment

--- src/main/java/domaindata/Comment.java
+++ src/main/java/domaindata/Comment.java
@@ -1,11 +1,13 @@
 package domaindata;
 
+import com.sun.istack.NotNull;
 import users.Person;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
 
 @Entity
 public class Comment {
@@ -15,11 +17,15 @@ public class Comment {
     @GeneratedValue
     private int id;
 
+    @NotNull
     private String content;
     @ManyToOne
     private Person commenter;
 
+    private LocalDateTime created;
+
     public Comment() {
+        created = LocalDateTime.now();
     }
 
     public String getContent() {

32f5d6ff89af5c0d3e3e17d0a2d1d2fe48beb6e2
TobiasStadler97 <tobias.stadler@hotmail.de>
2021-10-27 15:46:32 +0200

    UB-42: Added association between FacultyGC and Faculty

--- src/main/java/graphic/FacultyGC.java
+++ src/main/java/graphic/FacultyGC.java
@@ -1,9 +1,24 @@
 package graphic;
 
+import domaindata.Faculty;
+
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 
 @Entity
 public class FacultyGC extends GraphicalComponent {
+
+    @OneToOne
+    private Faculty faculty;
+
     public FacultyGC() {
+    }
+
+    public Faculty getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(Faculty faculty) {
+        this.faculty = faculty;
     }
 }

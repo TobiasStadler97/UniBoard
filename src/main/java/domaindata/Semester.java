df49fc9d9ea407f7b1611702eb6595bd7c9dbd88
TobiasStadler97 <tobias.stadler@hotmail.de>
2021-10-27 15:49:45 +0200

    UB-42: associations for Semester and Semestergc

--- src/main/java/domaindata/Semester.java
+++ src/main/java/domaindata/Semester.java
@@ -1,5 +1,8 @@
 package domaindata;
 
+import com.sun.istack.NotNull;
+import graphic.SemesterGC;
+
 import javax.persistence.*;
 
 @Entity
@@ -8,8 +11,13 @@ public class Semester {
     @GeneratedValue
     private int id;
 
+    @NotNull
     private int period;
+    @NotNull
     private int year;
+
+    @OneToOne
+    private SemesterGC semesterGC;
 
     @OneToOne
     private CommentArea commentArea;
@@ -31,5 +39,25 @@ public class Semester {
 
     public void setYear(int year) {
         this.year = year;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public SemesterGC getSemesterGC() {
+        return semesterGC;
+    }
+
+    public CommentArea getCommentArea() {
+        return commentArea;
+    }
+
+    public void setSemesterGC(SemesterGC semesterGC) {
+        this.semesterGC = semesterGC;
+    }
+
+    public void setCommentArea(CommentArea commentArea) {
+        this.commentArea = commentArea;
     }
 }
--- src/main/java/graphic/SemesterGC.java
+++ src/main/java/graphic/SemesterGC.java
@@ -1,11 +1,24 @@
 package graphic;
 
+import domaindata.Semester;
+
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 
 @Entity
 public class SemesterGC extends GraphicalComponent {
 
+    @OneToOne
+    private Semester semester;
 
     public SemesterGC() {
+    }
+
+    public Semester getSemester() {
+        return semester;
+    }
+
+    public void setSemester(Semester semester) {
+        this.semester = semester;
     }
 }

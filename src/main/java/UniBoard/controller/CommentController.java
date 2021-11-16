f2304018954d12f29f3024463dbe600fc0d8151c
TobiasStadler97 <tobias.stadler@hotmail.de>
2021-11-16 14:40:55 +0100

    UB-62: implemented CommentController.java + get all comments method

--- src/main/java/UniBoard/controller/CommentController.java
+++ src/main/java/UniBoard/controller/CommentController.java
@@ -1,4 +1,24 @@
 package UniBoard.controller;
 
+
+import UniBoard.entity.domaindata.Comment;
+import UniBoard.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @GetMapping("/comments") // not yet tested
+    Iterable<Comment> getAllComments() {
+        return commentService.findAll(Sort.by(Sort.Direction.ASC, "created"));
+    }
 }

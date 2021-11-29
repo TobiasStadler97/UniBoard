5d01e9cf1ffaaa44899db835a892340979e1bd88
TobiasStadler97 <tobias.stadler@hotmail.de>
2021-11-29 16:08:32 +0100

    implemented UserRepository.java

---
+++ src/main/java/UniBoard/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package UniBoard.repository;
+
+import UniBoard.entity.users.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}

cbe6aeea85b98b0d9f625cafe3709d0ace1a5405
TobiasStadler97 <tobias.stadler@hotmail.de>
2021-11-29 16:24:56 +0100

    UB-62: implemented CommentController.java without getcommentbydiscussionid

--- src/main/java/UniBoard/controller/CommentController.java
+++ src/main/java/UniBoard/controller/CommentController.java
@@ -6,9 +6,7 @@ import UniBoard.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -17,8 +15,15 @@ public class CommentController {
     @Autowired
     private CommentService commentService;
 
-    @GetMapping("/comments") // not yet tested
-    Iterable<Comment> getAllComments() {
-        return commentService.findAll(Sort.by(Sort.Direction.ASC, "created"));
+    //comment by discusison id is missing
+
+    @PostMapping("/comment")
+    void newComment(@RequestBody Comment newComment){
+        commentService.save(newComment);
+    }
+
+    @DeleteMapping("comment/{id}")
+    void deleteComment(@PathVariable int id){
+        commentService.deleteById(id);
     }
 }
